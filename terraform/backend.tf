9b4b31b0e423061ea17f02f895aaa7885d962b9d
noreshbatchu <146570669+noreshbatchu@users.noreply.github.com>
2024-08-26 09:38:02 +0530

    Update backend.tf

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "noreshstatebucket"  # Your S3 bucket name
+    bucket         = "noreshstatebucket"  
     key            = "state/terraform.tfstate"
     region         = "us-east-1"
     encrypt        = true
